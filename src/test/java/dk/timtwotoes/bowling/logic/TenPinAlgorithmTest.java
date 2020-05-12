5f04e2c4fda61185af7fc07ac618635ef2157ae0
Tim Wolff <timtwotoes@me.com>
2020-05-12 22:05:06 +0200

    Migrated to JUnit 5.

--- src/test/java/dk/timtwotoes/bowling/logic/TenPinAlgorithmTest.java
+++ src/test/java/dk/timtwotoes/bowling/logic/TenPinAlgorithmTest.java
@@ -1,8 +1,8 @@
 package dk.timtwotoes.bowling.logic;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TenPinAlgorithmTest {
     private final FrameFactory factory = new FrameFactory();
